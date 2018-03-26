9d526c14b6f21556635de1559ae182df49c42c4b
Masterton <zhengcloud@foxmail.com>
2018-03-26 13:32:20 +0800

    add core/phpspider.php constants.php demo.php

---
+++ demo/demo.php
@@ -0,0 +1,51 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+use phpspider\core\phpspider;
+use phpspider\core\requests;
+use phpspider\core\db;
+
+/* Do NOT delete this comment */
+/* 不要删除这段注释 */
+
+//$url = "https://istore.oppomobile.com/storeapp/home?size=10&start=0";
+//$data = requests::get($url);
+//$info = requests::$info;
+//print_r($info);
+//exit;
+
+$configs = array(
+    'name' => 'test',
+    'tasknum' => 1,
+    //'multiserver' => true,
+    'log_show' => true,
+    //'save_running_state' => false,
+    'domains' => array(
+        'www.test.com'
+    ),
+    'scan_urls' => array(
+        "http://www.test.com/qingchunmeinv/"
+    ),
+    'list_url_regexes' => array(
+        "http://www.test.com/qingchunmeinv/index_\d+.html"
+    ),
+    'content_url_regexes' => array(
+        "http://www.test.com/qingchunmeinv/\d+.html"
+    ),
+    //'export' => array(
+        //'type' => 'db', 
+        //'table' => 'meinv_content',
+    //),
+    'db_config' => array(
+        'host'  => '127.0.0.1',
+        'port'  => 3306,
+        'user'  => 'test',
+        'pass'  => 'test',
+        'name'  => 'test',
+    ),
+    'fields' => array(),
+);
+
+$spider = new phpspider($configs);
+print_r("<pre>");
+print_r($spider::$configs);
+exit;
