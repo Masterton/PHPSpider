cbd961a900be15f5735c70bc210d2ef15dc497fb
Masterton <zhengcloud@foxmail.com>
2018-05-09 13:50:23 +0800

    功能拆分 V1.1.0

---
+++ lib/Callback/CallbackParam.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @version 1.1.0
+ * @author Masterton <zhengcloud@foxmail.com>
+ * @time 2018-5-9 11:23:29
+ */
+
+namespace PHPSpider\Callback;
+
+/**
+ * CallbackParam
+ */
+class CallbackParam
+{
+    // TODO
+}
---
+++ lib/Redis/RedisClient.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @version 1.1.0
+ * @author Masterton <zhengcloud@foxmail.com>
+ * @time 2018-5-9 11:23:29
+ */
+
+namespace PHPSpider\Redis;
+
+/**
+ * RedisClient
+ */
+class RedisClient
+{
+    // TODO
+}
