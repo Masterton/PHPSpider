b6ef70634841f190977bb626f3c3d9c72f6504a1
Masterton <zhengcloud@foxmail.com>
2018-05-11 22:01:40 +0800

    回调 V1.1.0

--- lib/Callback/Callback.php
+++ lib/Callback/Callback.php
@@ -1,5 +1,11 @@
 <?php
 
+interface ICallbackNamed
+{
+    function hasName();
+    function getName();
+}
+
 /**
  * @version 1.1.0
  * @author Masterton <zhengcloud@foxmail.com>
@@ -11,7 +17,44 @@ namespace PHPSpider\Callback;
 /**
  * Callback
  */
-class Callback
+class Callback implements ICallbackName
 {
-    // TODO
+    public $callback = null;
+    public $params = null;
+    protected $name;
+
+    public function __construct($callback, $param1 = null, $param2 = null, $param3 = null)
+    {
+        $params = func_get_args();
+        $params = array_slice($params, 1);
+        if ($callback instanceof Callback) {
+            // TODO implement recurention
+        } else {
+            $this->callback = $callback;
+            $this->params = $params;
+        }
+    }
+
+    public function getName()
+    {
+        return 'Callback: ' . $this->name;
+    }
+
+    public function hasName()
+    {
+        return isset($this->name) && $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    // TODO test me
+    // public function addParams()
+    // {
+    //     $params = func_get_args();
+    //     return new Callback($this->callback, $this->params + $params);
+    // }
 }
