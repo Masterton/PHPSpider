c53a5b89f3644e3718be59e6c959eef433b46c8e
Masterton <zhengcloud@foxmail.com>
2018-07-24 21:10:26 +0800

    Event V1.1.0

--- lib/DOM/Event.php
+++ lib/DOM/Event.php
@@ -26,4 +26,29 @@ class Event
      * @var unknown_type
      */
     public $cancelable = true;
+
+    /**
+     * Returns a reference to the currently registered target for the event.
+     *
+     * @var unknown_type
+     */
+    public $currentTarget;
+
+    /**
+     * Returns detail about the event, depending on the type of event.
+     *
+     * @var unknown_type
+     * @link http://developer.mozilla.org/en/DOM/event.detail
+     */
+    public $detail; // ???
+
+    /**
+     * Used to indicate which phase of the event flow is currently being evaluated.
+     *
+     * NOT IMPLEMENTED
+     *
+     * @var unknown_type
+     * @link http://developer.mozilla.org/en/DOM/event.eventPhase
+     */
+    public $eventPhase; // ???
 }
