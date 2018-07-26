ab26ea6f9a91187c7cb38a5a5fecb58033243cca
Masterton <zhengcloud@foxmail.com>
2018-07-26 21:00:11 +0800

    DocumentWrapper V1.1.0

--- lib/DOM/DocumentWrapper.php
+++ lib/DOM/DocumentWrapper.php
@@ -18,4 +18,18 @@ class DocumentWrapper
      */
     public $document;
     public $id;
+
+    /**
+     * @todo Rewrite as method and quess if null.
+     *
+     * @var unknown_type
+     */
+    public $contentType = '';
+    public $xpath;
+    public $uuid = 0;
+    public $data = array();
+    public $dataNodes = array();
+    public $events = array();
+    public $eventsNodes = array();
+    public $eventsGlobal = array();
 }
