0d95df5bd34b3cb440a51e585cc569d5247c49ba
Masterton <zhengcloud@foxmail.com>
2018-08-04 18:22:23 +0800

    DocumentWrapper V1.1.0

--- lib/DOM/DocumentWrapper.php
+++ lib/DOM/DocumentWrapper.php
@@ -55,10 +55,10 @@ class DocumentWrapper
 
     public function __construct($markup = null, $contentType = null, $newDocumentID = null)
     {
-    	if (isset($markup)) {
-    		$this->load($markup, $contentType, $newDocumentID);
-    	}
-    	$this->id = $newDocumentID ? $newDocumentID : md5(microtime());
+        if (isset($markup)) {
+            $this->load($markup, $contentType, $newDocumentID);
+        }
+        $this->id = $newDocumentID ? $newDocumentID : md5(microtime());
     }
 
     /**
@@ -66,26 +66,26 @@ class DocumentWrapper
      */
     public function load($markup, $contentType = null, $newDocumentID = null)
     {
-    	// Query::$documents[$id] = $this;
-    	$this->contentType = strtolower($contentType);
-    	if ($markup instanceof DOMDOCUMENT) {
-    		$this->document = $markup;
-    		$this->root = $this->document;
-    		$this->charset = $this->document->encoding;
-    		// TODO isDocumentFragment
-    	} else {
-    		$loaded = $this->loadMarkup($markup);
-    	}
-    	if ($loaded) {
-    		// $this->document->formatOutput = true;
-    		$this->document->preserveWhiteSpace = true;
-    		$this->xpath = new DOMXpath($this->document);
-    		$this->afterMarkupLoad();
-    		return true;
-    		// remember last loaded document
-    		// return Query::selectDocument($id);
-    	}
-    	return false;
+        // Query::$documents[$id] = $this;
+        $this->contentType = strtolower($contentType);
+        if ($markup instanceof DOMDOCUMENT) {
+            $this->document = $markup;
+            $this->root = $this->document;
+            $this->charset = $this->document->encoding;
+            // TODO isDocumentFragment
+        } else {
+            $loaded = $this->loadMarkup($markup);
+        }
+        if ($loaded) {
+            // $this->document->formatOutput = true;
+            $this->document->preserveWhiteSpace = true;
+            $this->xpath = new DOMXpath($this->document);
+            $this->afterMarkupLoad();
+            return true;
+            // remember last loaded document
+            // return Query::selectDocument($id);
+        }
+        return false;
     }
 
     /**
@@ -93,9 +93,9 @@ class DocumentWrapper
      */
     protected function afterMarkupLoad()
     {
-    	if ($this->isXHTML) {
-    		$this->xpath->registerNamespace("html", "http://www.w3.org/1999/xhtml");
-    	}
+        if ($this->isXHTML) {
+            $this->xpath->registerNamespace("html", "http://www.w3.org/1999/xhtml");
+        }
     }
 
     /**
@@ -103,44 +103,44 @@ class DocumentWrapper
      */
     protected function loadMarkup($markup)
     {
-    	$loaded = false;
-    	if ($this->contentType) {
-    		self::debug("Load markup for content type {$this->contentType}");
-    		// content determined by contentType
-    		list($contentType, $charset) = $this->contentTypeToArray($this->contentType);
-    		switch ($contentType) {
-    			case 'text/html':
-    				Query::debug("Loading HTML, content type '{$this->contentType}'");
-    				$loaded = $this->loadMarkupHTML($markup, $charset);
-    				break;
-    			case 'text/html':
-    			case 'application/xhtml+xml':
-    				Query::debug("Loading XML, content type '{$this->contentType}'");
-    				$loaded = $this->loadMarkupXML($markup, $charset);
-    				break;
-    			default:
-    				// for feeds or anything that sometimes doesn't use text/xml
-    				if (strpos('xml', $this->contentType) !== false) {
-    					Query::debug("Loading XML, content type '{$this->contentType}'");
-    					$loaded = $this->loadMarkupXML($markup, $charset);
-    				} else {
-    					Query::debug("Could not determine document type from content type '{$this->contentType}'");
-    				}
-    		}
-    	} else {
-    		// content type autodetection
-    		if ($this->isXML($markup)) {
-    			Query::debug("Loading XML, isXML() == true");
-    			if (!$loaded && $this->isXHTML) {
-    				Query::debug('Loading as XML failed, trying to load as HTML, isXHTML == true');
-    				$loaded = $this->loadMarkupHTML($markup);
-    			}
-    		} else {
-    			Query::debug("Loading HTML, isXML() == false");
-    			$loaded = $this->loadMarkupHTML($markup);
-    		}
-    	}
-    	return $loaded;
+        $loaded = false;
+        if ($this->contentType) {
+            self::debug("Load markup for content type {$this->contentType}");
+            // content determined by contentType
+            list($contentType, $charset) = $this->contentTypeToArray($this->contentType);
+            switch ($contentType) {
+                case 'text/html':
+                    Query::debug("Loading HTML, content type '{$this->contentType}'");
+                    $loaded = $this->loadMarkupHTML($markup, $charset);
+                    break;
+                case 'text/html':
+                case 'application/xhtml+xml':
+                    Query::debug("Loading XML, content type '{$this->contentType}'");
+                    $loaded = $this->loadMarkupXML($markup, $charset);
+                    break;
+                default:
+                    // for feeds or anything that sometimes doesn't use text/xml
+                    if (strpos('xml', $this->contentType) !== false) {
+                        Query::debug("Loading XML, content type '{$this->contentType}'");
+                        $loaded = $this->loadMarkupXML($markup, $charset);
+                    } else {
+                        Query::debug("Could not determine document type from content type '{$this->contentType}'");
+                    }
+            }
+        } else {
+            // content type autodetection
+            if ($this->isXML($markup)) {
+                Query::debug("Loading XML, isXML() == true");
+                if (!$loaded && $this->isXHTML) {
+                    Query::debug('Loading as XML failed, trying to load as HTML, isXHTML == true');
+                    $loaded = $this->loadMarkupHTML($markup);
+                }
+            } else {
+                Query::debug("Loading HTML, isXML() == false");
+                $loaded = $this->loadMarkupHTML($markup);
+            }
+        }
+        return $loaded;
     }
 
     /**
@@ -148,7 +148,7 @@ class DocumentWrapper
      */
     protected function loadMarkupReset()
     {
-    	$this->isXML = $this->isXHTML = $this->isHTML = false;
+        $this->isXML = $this->isXHTML = $this->isHTML = false;
     }
 
     /**
@@ -156,14 +156,14 @@ class DocumentWrapper
      */
     protected function documentCreate($charset, $version = '1.0')
     {
-    	if (!$version) {
-    		$version = '1.0';
-    	}
-    	$this->document = new DOMDocument($version, $charset);
-    	$this->charset = $this->document->encoding;
-    	// $this->document->encoding = $charset;
-    	$this->document->formatOutput = true;
-    	$this->document->preserveWhiteSpace = true;
+        if (!$version) {
+            $version = '1.0';
+        }
+        $this->document = new DOMDocument($version, $charset);
+        $this->charset = $this->document->encoding;
+        // $this->document->encoding = $charset;
+        $this->document->formatOutput = true;
+        $this->document->preserveWhiteSpace = true;
     }
 
     /**
@@ -171,14 +171,81 @@ class DocumentWrapper
      */
     protected function loadMarkupHTML($markup, $requestedCharset = null)
     {
-    	if (Query::$debug) {
-    		Query::debug("Full markup load (HTML): " . substr($markup, 0, 250));
-    	}
-    	$this->loadMarkupReset();
-    	$this->isHTML = true;
-    	if (!isset($this->isDocumentFragment)) {
-    		$this->isDocumentFragment = self::isDocumentFragmentHTML($markup);
-    	}
-    	$charset = null;
+        if (Query::$debug) {
+            Query::debug("Full markup load (HTML): " . substr($markup, 0, 250));
+        }
+        $this->loadMarkupReset();
+        $this->isHTML = true;
+        if (!isset($this->isDocumentFragment)) {
+            $this->isDocumentFragment = self::isDocumentFragmentHTML($markup);
+        }
+        $charset = null;
+        $documentCharset = $this->charsetFromHTML($markup);
+        $addDocumentCharset = false;
+        if ($documentCharset) {
+            $charset = $documentCharset;
+            $markup = $this->charsetFixHTML($markup);
+        } else if ($requestedCharset) {
+           $charset = $requestedCharset;
+        }
+        if (!$charset) {
+            $charset = Query::$defaultCharset;
+        }
+        // HTTP 1.1 says that the default charset is ISO-8859-1
+        // @see http://www.w3.org/International/O-HTTP-charset
+        if (!$documentCharset) {
+            $documentCharset = 'ISO-8859-1';
+            $addDocumentCharset = true;
+        }
+        // Should be careful here, still need 'magic encoding detection' since lots of pages have other 'default encoding'
+        // Worse, some pages can have mixed encodings... we'll try not to worry about that
+        $requestedCharset = strtoupper($requestedCharset);
+        $documentCharset = strtoupper($documentCharset);
+        Query::debug("DOC: $documentCharset REQ: $requestedCharset");
+        if ($requestedCharset && $documentCharset && $requestedCharset !== $documentCharset) {
+            Query::debug("CHARSET CONVERT");
+            // Document Encoding Conversion
+            // http://code.google.com/p/phpquery/issues/detail?id=86
+            if (function_exists('mb_detect_encoding')) {
+                $possibleCharsets = array($documentCharset, $requestedCharset, 'AUTO');
+                $docEncoding = mb_detect_encoding($markup, implode(', ', $possibleCharsets));
+                if (!$docEncoding) {
+                    $docEncoding = $documentCharset; // ok trust the document
+                }
+                Query::debug("DETECTED '$docEncoding'");
+                // Detected does not match what document says...
+                if ($docEncoding !== $documentCharset) {
+                    // Tricky..
+                }
+                if ($docEncoding !== $requestedCharset) {
+                    Query::debug("CONVERT $docEncoding => $requestedCharset");
+                    $markup = mb_convert_encoding($markup, $requestedCharset, $docEncoding);
+                    $markup = $this->charsetAppendToHTML($markup, $requestedCharset);
+                    $charset = $requestedCharset;
+                }
+            } else {
+                Query::debug("TODO: charset conversion without mbstring...");
+            }
+        }
+        $return = false;
+        if ($this->isDocumentFragment) {
+            Query::debug("Full markup load (HTML), DocumentFragment detected, using charset '$charset'");
+            $return = $this->documentFragmentLoadMarkup($this, $charset, $markup);
+        } else {
+            if ($addDocumentCharset) {
+                Query::debug("Full markup load (HTML), appending charset: '$charset'");
+                $markup = $this->charsetAppendToHTML($markup, $charset);
+            }
+            Query::debug("Full markup load (HTML), documentCreate('$charset')");
+            $this->documentCreate($charset);
+            $return = Query::$debug === 2 ? $this->document->loadHTML($markup) : @$this->document->loadHTML($markup);
+            if ($return) {
+                $this->root = $this->document;
+            }
+        }
+        if ($return && ! $this->contentType) {
+            $this->contentType = 'text/html';
+        }
+        return $return;
     }
 }
