0dab6f21ff3d136ac295622a973b8bde4f8aa245
Masterton <zhengcloud@foxmail.com>
2018-09-02 19:46:47 +0800

    Redis V1.1.0

--- lib/Redis/Redis.php
+++ lib/Redis/Redis.php
@@ -31,4 +31,42 @@ class Redis
     public static $prefix = "phpspider";
 
     public static $error = "";
+
+    public static function init()
+    {
+        if (!extension_loaded("redis")) {
+            self::$error = "The redis extension was not found";
+            return false;
+        }
+
+        // 获取配置
+        $config = self::$link_name == 'default' ? self::_get_default_config() : self::$configs[self::$link_name];
+
+        // 如果当前链接标识符为空，或者ping不同，就close之后重新打开
+        //if ( empty(self::$links[self::$link_name]) || !self::ping() )
+        if (empty(self::$links[self::$link_name])) {
+            self::$links[self::$link_name] = new Redis();
+            if (!self::$links[self::$link_name]->connect($config['host'], $config['port'], $config['timeout'])) {
+                self::$error = "Unable to connect to redis server\nPlease check the configuration file config/inc_config.php";
+                unset(self::$links[self::$link_name]);
+                return false;
+            }
+
+            // 验证
+            if ($config['pass']) {
+                if (!self::$links[self::$link_name]->auth($config['pass'])) {
+                    self::$error = "Redis Server authentication failed\nPlease check the configuration file config/inc_config.php";
+                    unset(self::$links[self::$link_name]);
+                    return false;
+                }
+            }
+
+            $prefix = empty($config['prefix']) ? self::$prefix : $config['prefix'];
+            self::$links[self::$link_name]->setOption(Redis::OPT_PREFIX, $prefix . ":");
+            self::$links[self::$link_name]->setOption(Redis::OPT_READ_TIMEOUT, -1);
+            self::$links[self::$link_name]->select($config['db']);
+        }
+
+        return self::$links[self::$link_name];
+    }
 }
