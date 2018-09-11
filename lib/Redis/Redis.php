10afff8f92ef1b6035091d0b13b345d744637a8c
Masterton <zhengcloud@foxmail.com>
2018-09-11 22:02:05 +0800

    Redis V0.0.1

--- lib/Redis/Redis.php
+++ lib/Redis/Redis.php
@@ -255,4 +255,32 @@ class Redis
         $key = "Lock:{$name}";
         return self::del($key);
     }
+
+    /**
+     * get
+     * 
+     * @param mixed $key
+     * @return void
+     * @author seatle <seatle@foxmail.com> 
+     * @created time :2015-12-13 01:05
+     */
+    public static function get($key)
+    {
+        self::init();
+        try {
+            if (self::$links[self::$link_name]) {
+                return self::$links[self::$link_name]->get($key);
+            }
+        } catch (Exception $e) {
+            $msg = "PHP Fatal error:  Uncaught exception 'RedisException' with message '".$e->getMessage()."'\n";
+            log::warn($msg);
+            if ($e->getCode() == 0) {
+                self::$links[self::$link_name]->close();
+                self::$links[self::$link_name] = null;
+                usleep(100000);
+                return self::get($key);
+            }
+        }
+        return NULL;
+    }
 }
