94ab06c43cdc286176007dcb8664947655051ddb
Masterton <zhengcloud@foxmail.com>
2018-09-19 22:21:07 +0800

    Redis V0.0.1

--- lib/Redis/Redis.php
+++ lib/Redis/Redis.php
@@ -474,4 +474,32 @@ class Redis
         }
         return NULL;
     }
+
+    /**
+     * select 按索引查询
+     * 
+     * @param mixed $index
+     * @return void
+     * @author seatle <seatle@foxmail.com> 
+     * @created time :2015-12-18 11:28
+     */
+    public static function select($index)
+    {
+        self::init();
+        try {
+            if ( self::$links[self::$link_name] ) {
+                return self::$links[self::$link_name]->select($index);
+            }
+        } catch (Exception $e) {
+            $msg = "PHP Fatal error:  Uncaught exception 'RedisException' with message '".$e->getMessage()."'\n";
+            log::warn($msg);
+            if ($e->getCode() == 0) {
+                self::$links[self::$link_name]->close();
+                self::$links[self::$link_name] = null;
+                usleep(100000);
+                return self::select($index);
+            }
+        }
+        return NULL;
+    }
 }
