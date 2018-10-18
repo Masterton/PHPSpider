24105863c845eb8ad67adaacadb3dc1d864a9ce2
Masterton <zhengcloud@foxmail.com>
2018-10-18 20:25:08 +0800

    Redis V0.0.1

--- lib/Redis/RedisClient.php
+++ lib/Redis/RedisClient.php
@@ -44,4 +44,71 @@ class RedisClient
         }
         return $this->read_response();
     }
+
+    private function read_response() 
+    {
+        $reply = trim(fgets($this->redis_socket, 1024));
+        switch (substr($reply, 0, 1))
+        {
+        case '-':
+            throw new Exception(trim(substr($reply, 1)));
+            break;
+        case '+':
+            $response = substr(trim($reply), 1);
+            if ($response === 'OK') 
+            {
+                $response = TRUE;
+            }
+            break;
+        case '$':
+            $response = NULL;
+            if ($reply == '$-1') 
+            {
+                break;
+            }
+            $read = 0;
+            $size = intval(substr($reply, 1));
+            if ($size > 0) 
+            {
+                do 
+                {
+                    $block_size = ($size - $read) > 1024 ? 1024 : ($size - $read);
+                    $r = fread($this->redis_socket, $block_size);
+                    if ($r === FALSE) 
+                    {
+                        throw new Exception('Failed to read response from stream');
+                    }
+                    else 
+                    {
+                        $read += strlen($r);
+                        $response .= $r;
+                    }
+                }
+                while ($read < $size);
+            }
+            fread($this->redis_socket, 2); /* discard crlf */
+            break;
+            /* Multi-bulk reply */
+        case '*':
+            $count = intval(substr($reply, 1));
+            if ($count == '-1') 
+            {
+                return NULL;
+            }
+            $response = array();
+            for ($i = 0; $i < $count; $i++) 
+            {
+                $response[] = $this->read_response();
+            }
+            break;
+            /* Integer reply */
+        case ':':
+            $response = intval(substr(trim($reply), 1));
+            break;
+        default:
+            throw new RedisException("Unknown response: {$reply}");
+            break;
+        }
+        return $response;
+    }
 }
