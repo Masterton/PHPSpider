ddae29d13210046e45ca37bf91f87df5ccf08b31
Masterton <zhengcloud@foxmail.com>
2018-10-23 22:09:19 +0800

    RedisServer V0.0.1

--- lib/Redis/RedisServer.php
+++ lib/Redis/RedisServer.php
@@ -17,4 +17,11 @@ class RedisServer
     private $process_num = 3;
     public $redis_kv_data = array();
     public $onMessage = null;
+
+    public function __construct($host="0.0.0.0", $port=6379)
+    {
+        $this->socket = stream_socket_server("tcp://".$host.":".$port,$errno, $errstr);
+        if (!$this->socket) die($errstr."--".$errno);
+        echo "listen $host $port \r\n";
+    }
 }
