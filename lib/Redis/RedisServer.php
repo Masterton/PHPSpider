9037ef438aac800842850317df5ac05944f71d65
Masterton <zhengcloud@foxmail.com>
2018-10-24 21:50:35 +0800

    RedisServer V0.0.1

--- lib/Redis/RedisServer.php
+++ lib/Redis/RedisServer.php
@@ -24,4 +24,50 @@ class RedisServer
         if (!$this->socket) die($errstr."--".$errno);
         echo "listen $host $port \r\n";
     }
+
+    private function parse_resp(&$conn)
+    {
+        // 读取一行，遇到 \r\n 为一行
+        $line = fgets($conn);
+        if($line === '' || $line === false)
+        {
+            return null;
+        }
+        // 获取第一个字符作为类型
+        $type = $line[0];
+        // 去掉第一个字符，去掉结尾的 \r\n
+        $line = mb_substr($line, 1, -2);
+        switch ( $type )
+        {
+        case "*":
+            // 得到长度
+            $count = (int) $line;
+            $data = array();
+            for ($i = 1; $i <= $count; $i++) 
+            {
+                $data[] = $this->parse_resp($conn);
+            }
+            return $data;
+        case "$":
+            if ($line == '-1') 
+            {
+                return null;
+            }
+            // 截取的长度要加上 \r\n 两个字符
+            $length = $line + 2;
+            $data = '';
+            while ($length > 0) 
+            {
+                $block = fread($conn, $length);
+                if ($length !== strlen($block)) 
+                {
+                    throw new Exception('RECEIVING');
+                }
+                $data .= $block;
+                $length -= mb_strlen($block);
+            }
+            return mb_substr($data, 0, -2);
+        }
+        return $line;
+    }
 }
